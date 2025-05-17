5894a11cd7343c104fc71cf8337d153473a2d100
hoowlr13 <n34677665@gmail.com>
2025-05-17 17:04:39 +0900

    25_05_17 1주일차
    해결해야될 가장 큰문제는
    블럭이랑 보드상태가 나누어져있는데 한번에 관리하기
    이동할때마다 boardstate를 바꾸는것이 아닌 이동할때 boardstate = not fill이면 이동할수있도록 변경하기 이동할수없다면? fill로 boardstate변경

--- Tetris/main.py
+++ Tetris/main.py
@@ -1,45 +1,142 @@
-# 1일차 목표: 블럭 생성후 화살표로 이동
-# 2일차 목표: 테트리스 틀, 격자 생성 + 키 한번에 한칸씩 이동하게 설정하기
-import pygame
-import os
+import pygame as pg
+from pygame.locals import *
+import os, time, random
 
-xPos = 900
-yPos = 70
-os.environ['SDL_VIDEO_WINDOW_POS'] = f"{xPos},{yPos}"
+# 테트리스는 가로 10 세로 40의 크기로 구성됨
+# 한 칸의 크기는 가로 20 세로 20으로 구성됨
+# 화면의 크기는 200, 800으로 구성
+# 
+class App:
+    def __init__(self): # 여기에 사용할 변수, 함수등 다넣어놓고 사용
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 400, 800
+        self.board_x, self.board_y = 10, 20
+        self.EMPTY_board = 0
+        self.FILL_board = 1
+        self.board_state = [[self.EMPTY_board for _ in range(self.board_x)] for _ in range(self.board_y)] # 보드에있는 x, y를 격자 그리드로 설정하고 
+        self.bweight, self.bheight = (20, 20)
 
-pygame.init() # 초기화
-screen = pygame.display.set_mode((460, 740))
-clock = pygame.time.Clock()
-running = True
-
-# Settings
-defRect = (100, 100, 100, 100)
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-gravity = 0.8
-
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        self.curren_piece = True # 나중에 블럭의 모양으로 변경경
+        self.block_check = False
+        # 보드의 상태를 나타내는 그리드 완성 후 키보드로 이동하며 블록이 격자안에 채워지면 상태 바꾸기
         
+        self.move = self.moveX, self.moveY = (0, 0)
+        self.curren_piece = self.board_state[0][0]
+
+        xPos = 900
+        yPos = 50
+        os.environ['SDL_VIDEO_WINDOW_POS'] = f"{xPos},{yPos}"
+
+     
+
+    def on_init(self): #
+        pg.init() # 초기화
+        self._display_surf = pg.display.set_mode(self.size, pg.HWSURFACE | pg.DOUBLEBUF, vsync=1)
+        self._running = True
+
+        
+
+    def on_event(self, event):
+        if event.type == pg.QUIT:
+            self._running = False
+
+
+        keys = pg.key.get_pressed()
+        if event.type == pg.KEYDOWN: # 연속으로 입력되는 문제 o
+            if keys[pg.K_LEFT] and self.moveX > 0:
+
+                self.board_state[1][2] = self.FILL_board
+                self.moveX -= 20
+                print(f"{self.moveX} {self.moveY}")
+            elif keys[pg.K_RIGHT] and self.moveX < 180:
+                self.moveX += 20
+                
+            elif keys[pg.K_DOWN]:
+                self.moveY += 20
+                print(self.board_state[(self.moveY//20)-2][(self.moveX//20)-2])
+            self.board_state[(self.moveY//20)-1][(self.moveX//20)-1] = self.FILL_board
+        
+        
+
+
+# key값을 return받아서 on_block에서 블럭을 이동
+# 리턴을 어떻게 받아야할까
+
+
+    def on_board(self, width = int, height = int): # 테트리스 보드 그리기 
+        self.bsize = self.bweight, self.bheight 
+
+        for i in range(10):
+            for j in range(20):
+                newRect = pg.Rect(i*self.bweight, j*self.bheight, self.bweight, self.bheight)   
+                pg.draw.rect(self._display_surf, "black", newRect, 1) # 그다음부터가 
+
+
+    def on_block(self, event, bweight, bheight): # 테트리스 이동시키는 함수
+        self.current_type_rect = None # 현재 출력할 블록저장
+        block_type = ["I", "O"]
+        
+        """"Z": 3,
+            "S": 4,
+            "J": 5,
+            "L": 6,
+            "T": 7"""
+        
+        
+        if not self.block_check:
+            select = random.choice(block_type)
+            self.block_check = True
+            self.current_type = select
+        
+
+        if self.current_type == "I":
+            self.current_type_rect = pg.Rect(self.moveX, self.moveY, bweight, bheight*4)
+        elif self.current_type == "O":
+            self.current_type_rect = pg.Rect(self.moveX, self.moveY, bweight*2, bheight*2)
+
+        if self.current_type_rect: # draw
+            pg.draw.rect(self._display_surf, "blue", self.current_type_rect)
+            self.curren_piece = self.current_type
+
+        # 이 height가 0이될때까지 아래로 이동
+        # if not self.curren_piece:
+         
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        if (self.moveY//20) > 20:
+            self.moveY += 20
+        pg.display.flip()
+
+    def on_cleanup(self):
+        pg.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while (self._running):
+            self._display_surf.fill("white")
+            
+            for event in pg.event.get():
+                self.on_event(event)
+                
+            self.on_board()
+           
+            self.on_block(event, self.bweight, self.bheight)
+            self.curren_piece = True
+
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+
     
-    screen.fill("white")
-    pygame.draw.rect(screen, "black", (player_pos.x, player_pos.y, 30, 30))
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and not player_pos.y < 10:
-        player_pos.y -= 30 
-    if keys[pygame.K_DOWN] and not player_pos.y > 720:
-        player_pos.y += 30
-    if keys[pygame.K_LEFT] and not player_pos.x < 10:
-        player_pos.x -= 30
-    if keys[pygame.K_RIGHT] and not player_pos.x > 430:
-        player_pos.x += 30
-    if keys[pygame.K_r]:
-        pygame.init()
-        print("Sdf")
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
 
-    pygame.display.flip() # 화면업데이트트
-    clock.tick(20)  # limits FPS to 60
 
-pygame.quit()
